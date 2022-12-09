3550a289ce7aa25d51e9507b5e53551ee3a3e761
Victorydoreen <107753734+Victorydoreen@users.noreply.github.com>
2022-12-08 22:59:38 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+// const car={
+//     name:'FIAT',
+//     model:500,
+//     weight:300,
+//     color:'gray'
+// }
+// console.log(car.color);
+
+// const person={
+//     firstName:'John',
+//     lastName:'Doe',
+//     id:22,
+//     fullName: function(){
+//         return 
+//         this.firstName+ ""+ this.lastName;
+//     }
+// };
+//  console.log(person.id)
+// // console.log(fullName)
+
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe"
+//    };
+// console.log(alert(person.firstName))   
+
+
+// let n='Genesis'
+// let length=n.length
+// console.log(length)
+
+let text = "We are the so-called \"Vikings\" from the north."
+let length=text.length
+console.log(length)
+
+let k= 'It\'s alright.'
+console.log(k)
+
+let m = "The character \\ is called backslash.";
+console.log(m)
+
+let fruit = "Apple, Banana, Kiwi";
+let part = fruit.slice(7,13);
+console.log(part)
+
+let h = fruit.substr(7,6);
+console.log(h)
+
+let x=fruit.replace('Apple','Orange')
+console.log(x)
+
+let y=fruit.replaceAll(","," + ")
+console.log(y)
